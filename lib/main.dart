686d6d35891aac48c2f6952810693dfcae06f254
DanielZiborov <ziborovdanila5@gmail.com>
2024-11-02 21:28:59 +0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
   }
 }
